d5a2f40c9d0c84e0b41d873a76c1334ca1783621
HMosque <hector.mosquera@nestle.com>
2025-07-07 10:06:39 +0200

    docker files and requeriments for apps, just folder structure complete

---
+++ apps/create-campaign/main.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == "__main__":
+    print("Starting Create Campaign...")
+    print("1. Fetching data from JIRA...")
+    
+
+    print("JOB DONE: Create Campaign completed successfully.")
---
+++ apps/evaluate-release/main.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == "__main__":
+    print("Starting Eval release...")
+    print("1. Fetching data from JIRA...")
+    
+
+    print("JOB DONE: Eval release completed successfully.")
